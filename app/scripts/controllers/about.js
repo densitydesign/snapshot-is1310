13949760580f351c6f07ef940d3c6038f353d22e
iosonosempreio <tommaso.elli@gmail.com>
2016-04-26 19:24:08 +0200

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rolApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the rolApp
+ */
+angular.module('rolApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
